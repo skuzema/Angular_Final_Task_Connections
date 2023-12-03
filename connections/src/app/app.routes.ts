72ae97d088dbd707ecfbc3eebb2741a619b6281b
Sergejs Kuzema <s.kuzema@gmail.com>
2023-12-03 16:39:48 +0200

    feat: 404 page

--- connections/src/app/app.routes.ts
+++ connections/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from "@angular/router";
 
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+
 export const routes: Routes = [
     { path: "", redirectTo: "main", pathMatch: "full" },
     {
         path: "main",
-        loadChildren: () =>
+        loadComponent: () =>
             import("./pages/main-page/main-page.component").then(
                 (m) => m.MainPageComponent
             ),
     },
+    { path: "**", component: NotFoundPageComponent },
 ];
---
+++ connections/src/app/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: "app-not-found-page",
+    standalone: true,
+    imports: [MatButtonModule],
+    templateUrl: "./not-found-page.component.html",
+    styleUrl: "./not-found-page.component.scss",
+})
+export class NotFoundPageComponent {
+    constructor(private router: Router) {}
+
+    onGoToMain(): void {
+        this.router.navigate(["/main"]);
+    }
+}
