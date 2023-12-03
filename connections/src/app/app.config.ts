d84a20e816fe8f49cca51b23e1d76301a84ed718
Sergejs Kuzema <s.kuzema@gmail.com>
2023-12-03 11:55:26 +0200

    fix: linting

--- connections/src/app/app.config.ts
+++ connections/src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, isDevMode } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
+import { provideEffects } from "@ngrx/effects";
+import { provideRouterStore } from "@ngrx/router-store";
+import { provideStore } from "@ngrx/store";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
-import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideRouterStore } from '@ngrx/router-store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideEffects(), provideRouterStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideAnimations()]
+    providers: [
+        provideRouter(routes),
+        provideStore(),
+        provideEffects(),
+        provideRouterStore(),
+        provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+        provideAnimations(),
+    ],
 };
