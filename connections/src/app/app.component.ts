0ae36647149e1dd51eab721ca8ad479b6b8304eb
Sergejs Kuzema <s.kuzema@gmail.com>
2023-12-03 14:12:20 +0200

    feat: add main page

--- connections/src/app/app.component.ts
+++ connections/src/app/app.component.ts
@@ -2,12 +2,15 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
+import { HeaderComponent } from "./core/components/header/header.component";
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+
 @Component({
     selector: "app-root",
     standalone: true,
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MainPageComponent],
     templateUrl: "./app.component.html",
-    styleUrl: "./app.component.scss"
+    styleUrl: "./app.component.scss",
 })
 export class AppComponent {
     title = "connections";
--- connections/src/app/app.routes.ts
+++ connections/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "main", pathMatch: "full" },
+    {
+        path: "main",
+        loadChildren: () =>
+            import("./pages/main-page/main-page.component").then(
+                (m) => m.MainPageComponent
+            ),
+    },
+];
