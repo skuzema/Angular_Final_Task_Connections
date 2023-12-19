2878c7e1d7b5842510068e9f72bd9ecfda905657
skuzema <s.kuzema@gmail.com>
2023-12-19 18:52:06 +0200

    feat: highlight record

--- connections/src/app/shared/directives/highlight-conversation-id.directive.ts
+++ connections/src/app/shared/directives/highlight-conversation-id.directive.ts
@@ -1,27 +1,23 @@
-import { Directive, ElementRef, HostListener, Inject, Input } from '@angular/core';
-import { PeopleWithConversation } from '../models/data';
-import { Observable } from 'rxjs';
+import { Directive, ElementRef, Input, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
-  selector: '[highlightConversationId]',
+  selector: '[highlightConv]',
   standalone: true
 })
 export class HighlightConversationIdDirective {
-  @Input() people!: PeopleWithConversation;
+  @Input() highlightConv!: string | undefined;
 
   constructor(
-    @Inject('peoples$') private peoples$: Observable<PeopleWithConversation[]>,
-    private el: ElementRef
+    private el: ElementRef, private renderer: Renderer2
   ) { }
 
-  ngOnInit() {
-    this.peoples$.subscribe(people => {
-      this.people = people[0];
-      if (this.people.conversationId) {
-        this.el.nativeElement.style.backgroundColor = '#f0f0f0';
-      } else {
-        this.el.nativeElement.style.backgroundColor = '';
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("HighlightConversationIdDirective, changes:", changes);
+    if ("highlightConv" in changes) {
+      if (!this.highlightConv) {
+        return;
       }
-    });
+      this.el.nativeElement.style.backgroundColor = "#f0f0f0";
+    }
   }
 }
